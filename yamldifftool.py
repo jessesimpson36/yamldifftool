d92264d792aed8a905d0933ca44e79a2702a6bdd
Jesse Simpson <jesse.simpson@camunda.com>
2023-08-22 08:32:03 -0400

    added strict option to control how options get dropped

--- yamldifftool.py
+++ yamldifftool.py
@@ -11,12 +11,14 @@ def traverse_and_set(user_provided, root_diff, path):
         k = k.get(item)
     k[path[-1]] = user_provided
 
-def filter_defaults(base, user_provided, root_diff, path=[]):
+def filter_defaults(base, user_provided, root_diff, path=[], strict=False):
     if type(user_provided) is dict:
         parent_path=path
         for key in user_provided.keys():
-            if key in base.keys():
-                filter_defaults(base.get(key), user_provided.get(key), root_diff, path=path + [key])
+            if not strict and key not in base.keys():
+                traverse_and_set(user_provided, root_diff, path + [key])
+            elif key in base.keys():
+                filter_defaults(base.get(key), user_provided.get(key), root_diff, path=path + [key], strict=strict)
     elif type(user_provided) is list:
         if user_provided != base:
             traverse_and_set(user_provided, root_diff, path)
@@ -35,6 +37,7 @@ parser = argparse.ArgumentParser(description="Tool to identify diffs between val
 parser.add_argument("default_values", metavar="default_values.yaml", type=str, help="Base values.yaml for the helm chart version you're using")
 parser.add_argument("user_customized_values", metavar="customized.yaml", type=str, help="Customized values.yaml")
 parser.add_argument("-o", "--output", default="output", type=str, help="Write to filename. Otherwise default is output")
+parser.add_argument("-s", "--strict", default=False, action=argparse.BooleanOptionalAction, type=bool, help="strict mode will drop all options not in default values.yaml.")
 args = parser.parse_args()
 
 
@@ -44,7 +47,8 @@ with open(args.default_values, 'r') as default_values:
 with open(args.user_customized_values, 'r') as overwritten_values:
     overwritten_v = yaml.safe_load(overwritten_values)
 
-filter_defaults(default_v, overwritten_v, root_diff, path)
+
+filter_defaults(default_v, overwritten_v, root_diff, path, args.strict)
 
 with open(args.output, 'w') as outfile:
     yaml.dump(root_diff, outfile)
